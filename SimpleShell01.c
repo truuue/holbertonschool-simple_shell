3408986aa4bbba423252ccf41e08e968065c5789
sixen31 <alexisgrandjean@hotmail.fr>
2023-04-20 07:48:07 -0700

    add exit

--- SimpleShell01.c
+++ SimpleShell01.c
@@ -8,34 +8,35 @@
 
 int main(void)
 {
-        size_t len = 0;
-        ssize_t nread;
-        char *line = NULL;
-        char *argv[1024];
-        int count = 0, pid = 0, status;
-        char *token;
+	size_t len = 0;
+	ssize_t nread;
+	char *line = NULL;
+	char *argv[1024];
+	int count, pid, status;
+	char *token;
 
-        while ((nread = getline(&line, &len, stdin)) != -1)
-        {
-                pid = fork();
-                if(pid == 0)
-                {
-                        count = 0;
-
-                        token = strtok(line, " \n");
-                        while (token != NULL)
-                        {
-                                argv[count++] = token;
-                                token = strtok(NULL, " \n");
-                        }
-                        execve(argv[0], argv, NULL);
-                }
-                else
-                {
-                        wait(&status);
-                }
-        }
-        free(line);
-        return(0);
+	while ((nread = getline(&line, &len, stdin)) != -1)
+	{
+		pid = fork();
+		if (pid == -1) {
+			perror("fork");
+			exit(EXIT_FAILURE);
+		} else if (pid == 0) {
+			count = 0;
+			token = strtok(line, " \n");
+			while (token != NULL && count < 1024) {
+				argv[count++] = token;
+				token = strtok(NULL, " \n");
+			}
+			argv[count] = NULL;
+			execvp(argv[0], argv);
+			perror("execvp");
+			exit(EXIT_FAILURE);
+		} else {
+			wait(&status);
+		}
+	}
+	free(line);
+	return 0;
 }
 
