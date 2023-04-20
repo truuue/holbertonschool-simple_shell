3c9b7a75e75bb80276acece678c7d382572e094f
sixen31 <alexisgrandjean@hotmail.fr>
2023-04-20 02:42:04 -0700

    Create a code of the Simple Shell task2

---
+++ SimpleShell01.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(void)
+{
+	size_t len = 0;
+	ssize_t nread;
+	char *line = NULL;
+	char *argv[1024];
+	int count = 0, pid = 0, status;
+	char *token;
+
+	while ((nread = getline(&line, &len, stdin)) != -1)
+	{
+		pid = fork();
+		if(pid == 0)
+		{
+			count = 0;
+
+			token = strtok(line, " \n");
+			while (token != NULL)
+			{
+				argv[count] = token;
+				count++;
+				token = strtok(NULL, " \n");
+			}
+			execve(argv[0], argv, NULL);
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
+	return(0);
+}
