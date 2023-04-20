a02792ee1e2695871d6591e31749c0eea9558f29
truuue <noahvernhet@gmail.com>
2023-04-20 07:40:51 -0700

    Test simple shell for valgrind error

--- SimpleShell01.c
+++ SimpleShell01.c
@@ -8,34 +8,47 @@
 
 int main(void)
 {
-        size_t len = 0;
-        ssize_t nread;
-        char *line = NULL;
-        char *argv[1024];
-        int count = 0, pid = 0, status;
-        char *token;
+	size_t len = 0;
+	ssize_t nread;
+	char *line = NULL;
+	char *argv[1024];
+	int count = 0, pid = 0, status;
+	char *token;
 
-        while ((nread = getline(&line, &len, stdin)) != -1)
-        {
-                pid = fork();
-                if(pid == 0)
-                {
-                        count = 0;
+	while ((nread = getline(&line, &len, stdin)) != -1)
+	{
+		pid = fork();
+		if(pid == 0)
+		{
+			count = 0;
 
-                        token = strtok(line, " \n");
-                        while (token != NULL)
-                        {
-                                argv[count++] = token;
-                                token = strtok(NULL, " \n");
-                        }
-                        execve(argv[0], argv, NULL);
-                }
-                else
-                {
-                        wait(&status);
-                }
-        }
-        free(line);
-        return(0);
+			token = strtok(line, " \n");
+			while (token != NULL)
+			{
+				argv[count] = token;
+				count++;
+				token = strtok(NULL, " \n");
+			}
+			argv[count] = NULL; // Marqueur de fin de tableau pour execve
+
+			if(argv[0] != NULL) // Vérification que le pointeur n'est pas nul
+			{
+				execve(argv[0], argv, NULL);
+				perror("execve"); // En cas d'erreur dans execve
+				exit(EXIT_FAILURE);
+			}
+			else
+			{
+				fprintf(stderr, "Erreur : commande vide\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			waitpid(pid, &status, 0);
+		}
+	}
+	free(line);
+	line = NULL;
+	return(0);
 }
-
