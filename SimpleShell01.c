4fb591329809c031667b1553f1dda3ce3a30665a
sixen31 <alexisgrandjean@hotmail.fr>
2023-04-21 07:33:38 -0700

    Gestion option shell

--- SimpleShell01.c
+++ SimpleShell01.c
@@ -6,38 +6,43 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main(void)
+int main(int ac, char *av[], char *envp[])
 {
+	int MAX_ARGS = 1024;
 	size_t len = 0;
 	ssize_t nread;
 	char *line = NULL;
-	char *argv[1024];
+	char *argv[MAX_ARGS];
 	int count = 0, pid = 0, status;
-	char *token;
 
 	while ((nread = getline(&line, &len, stdin)) != -1)
 	{
-		pid = fork();
-		if(pid == 0)
-		{
-			count = 0;
+		count = 0;
 
-			token = strtok(line, " \n");
-			if(token == NULL)
-				continue;
-			while (token != NULL)
-			{
-				argv[count++] = token;
-				token = strtok(NULL, " \n");
-			}
-			execve("/bin/ls", argv, NULL);
+		char *token = strtok(line, " \n");
+		if (token == NULL)
+			continue;
+		while (token != NULL)
+		{
+			argv[count] = token;
+			count++;
+			token = strtok(NULL, " \n");
+		}
+		argv[count] = NULL;
+
+		pid = fork();
+		if (pid == 0)
+		{
+			execvp(argv[0], argv);
+			fprintf(stderr, "Erreur: commande introuvable\n");
+			exit(1);
 		}
 		else
 		{
 			wait(&status);
 		}
 	}
-	free(line);
-	return(0);
-}
 
+	free(line);
+	return 0;
+}
