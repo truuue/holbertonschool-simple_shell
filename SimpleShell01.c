30c57bb022e87bcc84ab552563b23f40df44dd04
sixen31 <alexisgrandjean@hotmail.fr>
2023-04-24 04:54:07 -0700

    Gestion des commandes

--- SimpleShell01.c
+++ SimpleShell01.c
@@ -6,7 +6,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main(void)
+int main(int ac, char *av[], char *envp[])
 {
 	size_t len = 0;
 	ssize_t nread;
@@ -14,23 +14,29 @@ int main(void)
 	char *argv[1024];
 	int count = 0, pid = 0, status;
 	char *token;
+	(void)ac;
+	(void)envp;
+	(void)av;
 
 	while ((nread = getline(&line, &len, stdin)) != -1)
 	{
+		count = 0;
+		token = strtok(line, " \n");
+		if (token == NULL)
+			continue;
+		while (token != NULL)
+		{
+			argv[count] = token;
+			count++;
+			token = strtok(NULL, " \n");
+		}
+		argv[count] = NULL;
 		pid = fork();
 		if (pid == 0)
 		{
-			count = 0;
-
-			token = strtok(line, " \n");
-			if(token == NULL)
-				continue;
-			while (token != NULL)
-			{
-				argv[count++] = token;
-				token = strtok(NULL, " \n");
-			}
-			execve(argv[0], argv, NULL);
+			execvp(argv[0], argv);
+			fprintf(stderr, "Erreur: commande introuvable\n");
+			exit(1);
 		}
 		else
 		{
@@ -38,5 +44,6 @@ int main(void)
 		}
 	}
 	free(line);
-	return(0);
+	return 0;
 }
+
