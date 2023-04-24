892b6fec878c9f7b8edd7670ca0076e18160baf6
truuue <noahvernhet@gmail.com>
2023-04-24 06:33:56 -0700

    Change for an older commit

--- SimpleShell01.c
+++ SimpleShell01.c
@@ -10,29 +10,26 @@ int main(int ac, char *av[1024], char *envp[])
 {
 	size_t len = 0;
 	ssize_t nread;
-	char *line = NULL, *token;
-	char command[1024];
+	char *line = NULL;
 	int count = 0, pid = 0, status;
-	(void) ac;
 
 	while ((nread = getline(&line, &len, stdin)) != -1)
 	{
+		count = 0;
+		char *token = strtok(line, " \n");
+		if (token == NULL)
+			continue;
+		while (token != NULL)
+		{
+			av[count] = token;
+			count++;
+			token = strtok(NULL, " \n");
+		}
+		av[count] = NULL;
 		pid = fork();
 		if (pid == 0)
 		{
-			count = 0;
-			token = strtok(line, " \n");
-			if (token == NULL)
-				continue;
-			while (token != NULL)
-			{
-				av[count] = token;
-				count++;
-				token = strtok(NULL, " \n");
-			}
-			av[count] = NULL;
-			sprintf(command, "/bin/%s", av[0]);
-			execve(command, av, envp);
+			execve(av[0], av, NULL);
 			fprintf(stderr, "Erreur: commande introuvable\n");
 			exit(1);
 		}
