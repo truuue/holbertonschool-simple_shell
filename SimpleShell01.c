41d44fd0718ac48966d80675e66c10c7225a4678
truuue <noahvernhet@gmail.com>
2023-04-24 01:45:01 -0700

    Change the code

--- SimpleShell01.c
+++ SimpleShell01.c
@@ -12,26 +12,25 @@ int main(void)
 	ssize_t nread;
 	char *line = NULL;
 	char *argv[1024];
-	char *token;
 	int count = 0, pid = 0, status;
+	char *token;
 
 	while ((nread = getline(&line, &len, stdin)) != -1)
 	{
-		count = 0;
-		token = strtok(line, " \n");
-		if(token == NULL)
-			continue;
-		while (token != NULL)
-		{
-			argv[count++] = token;
-			token = strtok(NULL, " \n");
-		}
-		argv[count] = NULL;
 		pid = fork();
 		if (pid == 0)
 		{
+			count = 0;
+
+			token = strtok(line, " \n");
+			if(token == NULL)
+				continue;
+			while (token != NULL)
+			{
+				argv[count++] = token;
+				token = strtok(NULL, " \n");
+			}
 			execve(argv[0], argv, NULL);
-			fprintf(stderr, "Erreur: commande introuvable\n");
 		}
 		else
 		{
