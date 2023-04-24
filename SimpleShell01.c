3e771a68baf98b588f04c306b9287c306b35d690
truuue <noahvernhet@gmail.com>
2023-04-24 00:40:54 -0700

    Correct the code

--- SimpleShell01.c
+++ SimpleShell01.c
@@ -6,34 +6,30 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main(int ac, char *av[], char *envp[])
+int main(int ac, char *av[1024], char *envp[])
 {
-	int MAX_ARGS = 1024;
 	size_t len = 0;
 	ssize_t nread;
 	char *line = NULL;
-	char *argv[MAX_ARGS];
 	int count = 0, pid = 0, status;
 
 	while ((nread = getline(&line, &len, stdin)) != -1)
 	{
 		count = 0;
-
 		char *token = strtok(line, " \n");
 		if (token == NULL)
 			continue;
 		while (token != NULL)
 		{
-			argv[count] = token;
+			av[count] = token;
 			count++;
 			token = strtok(NULL, " \n");
 		}
-		argv[count] = NULL;
-
+		av[count] = NULL;
 		pid = fork();
 		if (pid == 0)
 		{
-			execvp(argv[0], argv);
+			execve(av[0], av, NULL);
 			fprintf(stderr, "Erreur: commande introuvable\n");
 			exit(1);
 		}
@@ -42,7 +38,6 @@ int main(int ac, char *av[], char *envp[])
 			wait(&status);
 		}
 	}
-
 	free(line);
 	return 0;
 }
