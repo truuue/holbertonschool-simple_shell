9ee0e45538287bea4b4593ba9adb5b435b11be59
truuue <noahvernhet@gmail.com>
2023-04-25 01:49:52 -0700

    Test another version

--- SimpleShell01.c
+++ SimpleShell01.c
@@ -5,37 +5,34 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
 
 int main(int ac, char *av[1024], char *envp[])
 {
 	size_t len = 0;
 	ssize_t nread;
-	char *line = NULL, *token;
-	char command[1024];
+	char *line = NULL, *token, command[1024];
 	int count = 0, pid = 0, status;
 	(void) ac;
 
 	while ((nread = getline(&line, &len, stdin)) != -1)
 	{
+		count = 0;
+		token = strtok(line, " \n");
+		if(token == NULL)
+			continue;
+		while (token != NULL)
+		{
+			av[count] = token;
+			count++;
+			token = strtok(NULL, " \n");
+		}
+		av[count] = NULL;
 		pid = fork();
 		if (pid == 0)
 		{
-			count = 0;
-			token = strtok(line, " \n");
-			if (token == NULL)
-				continue;
-			while (token != NULL)
-			{
-				av[count] = token;
-				count++;
-				token = strtok(NULL, " \n");
-			}
-			av[count] = NULL;
 			sprintf(command, "/bin/%s", av[0]);
 			execve(command, av, envp);
-			fprintf(stderr, "Error: command not found\n");
-			exit(1);
+			fprintf(stderr, "Erreur: commande introuvable\n");
 		}
 		else
 		{
@@ -43,5 +40,5 @@ int main(int ac, char *av[1024], char *envp[])
 		}
 	}
 	free(line);
-	return (0);
+	return(0);
 }
