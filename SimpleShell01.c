4b3dc8e086589f85da5fd44b9103110fc8a8e7c1
sixen31 <alexisgrandjean@hotmail.fr>
2023-04-25 01:16:48 -0700

    SimpleShell a jour fonctionnel

--- SimpleShell01.c
+++ SimpleShell01.c
@@ -6,51 +6,41 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main(int ac, char *av[], char *envp[])
+int main(int ac, char *av[1024], char *envp[])
 {
-	size_t len = 0;
-	ssize_t nread;
-	char *line = NULL;
-	char *argv[1024];
-	int count = 0, pid = 0, status;
-	char *token;
-	(void)ac;
-	(void)envp;
-	(void)av;
+        size_t len = 0;
+        ssize_t nread;
+        char *line = NULL, *token, command[1024];
+        int count = 0, pid = 0, status;
+        (void)ac;
 
-	while ((nread = getline(&line, &len, stdin)) != -1)
-	{
-		count = 0;
-		token = strtok(line, " \n");
-		if (token == NULL)
-			continue;
-		while (token != NULL)
-		{
-			argv[count] = token;
-			count++;
-			token = strtok(NULL, " \n");
-		}
-		argv[count] = NULL;
-		pid = fork();
-		if (pid == 0)
-		{
-			execvp(argv[0], argv);
-			fprintf(stderr, "Erreur: commande introuvable\n");
-			exit(1);
-		}
-		else
-		{
-			wait(&status);
-		}
-	}
-	free(line);
-	return 0;
+        while ((nread = getline(&line, &len, stdin)) != -1)
+        {
+                count = 0;
+                token = strtok(line, " \n");
+                if (token == NULL)
+                        continue;
+                while (token != NULL)
+                {
+                        av[count] = token;
+                        count++;
+                        token = strtok(NULL, " \n");
+                }
+                av[count] = NULL;
+                pid = fork();
+                if (pid == 0)
+                {
+			sprintf(command, "/bin/%s", av[0]);
+                        execve(command, av, envp);
+                        fprintf(stderr, "Erreur: commande introuvable\n");
+                        exit(1);
+                }
+                else
+                {
+                        wait(&status);
+                }
+        }
+        free(line);
+        return 0;
 }
-
-
-
-
-
-
-
 
