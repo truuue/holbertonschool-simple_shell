169ef8e99a3ac1a1114a398d15d02d47e1e481b0
sixen31 <alexisgrandjean@hotmail.fr>
2023-04-25 06:17:31 -0700

    Add stat in the code

--- SimpleShell01.c
+++ SimpleShell01.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-#include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <sys/wait.h>
 
 int main(int ac, char *av[1024], char *envp[])
@@ -14,6 +14,8 @@ int main(int ac, char *av[1024], char *envp[])
         char *path = getenv("PATH");
         char new_path[1024];
         int count = 0, pid = 0, status;
+        struct stat stat_buf;
+
         (void)ac;
 
         while ((nread = getline(&line, &len, stdin)) != -1)
@@ -29,12 +31,23 @@ int main(int ac, char *av[1024], char *envp[])
                         token = strtok(NULL, " \n");
                 }
                 av[count] = NULL;
+
                 pid = fork();
                 if (pid == 0)
                 {
                         sprintf(command, "/bin/%s", av[0]);
                         sprintf(new_path, "%s:%s", path, "/bin");
                         setenv("PATH", new_path, 1);
+
+                        if (stat(command, &stat_buf) == -1) {
+                                fprintf(stderr, "Error: file not found or not accessible\n");
+                                exit(1);
+                        }
+                        if (access(command, X_OK) == -1) {
+                                fprintf(stderr, "Error: file not executable\n");
+                                exit(1);
+                        }
+
                         execve(command, av, envp);
                         fprintf(stderr, "Erreur: commande introuvable\n");
                         exit(1);
