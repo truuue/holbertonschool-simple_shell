331dd740337d3f200cde0478564bda02eee3fe08
sixen31 <alexisgrandjean@hotmail.fr>
2023-04-26 04:46:57 -0700

    Repar code check 1 , 2 et 3

--- simple_shell.c
+++ simple_shell.c
@@ -1,39 +1,53 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-int main(int ac, char *av[1024], char *envp[])
+int main(int argc, char *argv[], char *envp[])
 {
-        size_t len = 0;
-        ssize_t nread;
-        char *line = NULL, *token;
-        char command[1024];
-        int count = 0, pid = 0, status;
-        (void) ac;
+    size_t len = 0;
+    ssize_t nread;
+    char *line = NULL, *token;
+    char command[1024];
+    int status;
+    (void)argc;
 
-        while ((nread = getline(&line, &len, stdin)) != -1)
+    while ((nread = getline(&line, &len, stdin)) != -1)
+    {
+        int count = 0;
+        pid_t pid = fork();
+        if (pid == 0)
         {
-                pid = fork();
-                if (pid == 0)
-                {
-                        count = 0;
-                        token = strtok(line, " \n");
-                        if (token == NULL)
-                                continue;
-                        while (token != NULL)
-                        {
-                                av[count] = token;
-                                count++;
-                                token = strtok(NULL, " \n");
-                        }
-                        av[count] = NULL;
-                        sprintf(command, "/bin/%s", av[0]);
-                        execve(command, av, envp);
-                        fprintf(stderr, "Error: command not found\n");
-                }
-                else
-                {
-                        wait(&status);
-                }
+            token = strtok(line, " \n");
+            if (token == NULL)
+                continue;
+            while (token != NULL)
+            {
+                argv[count] = token;
+                count++;
+                token = strtok(NULL, " \n");
+            }
+            argv[count] = NULL;
+            if (strcmp(argv[0], "ls") == 0)
+            {
+                strcpy(command, "/bin/ls");
+                argv[0] = command;
+            }
+            execve(argv[0], argv, envp);
+            fprintf(stderr, "Error: command not found\n");
+            exit(EXIT_FAILURE);
         }
-        free(line);
-        return (0);
+        else if (pid < 0)
+        {
+            fprintf(stderr, "Error: failed to fork process\n");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            waitpid(pid, &status, 0);
+        }
+    }
+    free(line);
+    return (0);
 }
